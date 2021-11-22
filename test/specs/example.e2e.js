98fb9a87476d09b330294351ffa2d780163853a1
Vijay M <vijaym@Administrators-MacBook-Pro.local>
2021-11-22 20:59:20 +0530

    Added Webdriver IO and Api testing code

--- test/specs/example.e2e.js
+++ test/specs/example.e2e.js
@@ -1,50 +1,50 @@
-// import LoginPage from  '../pageobjects/login.page';
-// import LandingPage from '../pageobjects/landing.page';
-// import NewEmployeePage from '../pageobjects/new_employee.page';
+import LoginPage from  '../pageobjects/login.page';
+import LandingPage from '../pageobjects/landing.page';
+import NewEmployeePage from '../pageobjects/new_employee.page';
 var Client = require('node-rest-client').Client;
 
 describe('Swimlane application', () => {
-    // it('should login with valid credentials', async () => {
-    //     await LoginPage.open();
-    //     await LoginPage.login('vijay.bhasker', 'Q^crpXMCD7G5');
-    //     await expect(LandingPage.navigationBar).toBeExisting();
-    //     console.log(await LandingPage.navigationBar.getText());
-    //     await expect(LandingPage.navigationBar).toHaveTextContaining(
-    //         'APPLICATION RECORDS');
-    // });
+    it('should login with valid credentials', async () => {
+        await LoginPage.open();
+        await LoginPage.login('vijay.bhasker', 'Q^crpXMCD7G5');
+        await expect(LandingPage.navigationBar).toBeExisting();
+        console.log(await LandingPage.navigationBar.getText());
+        await expect(LandingPage.navigationBar).toHaveTextContaining(
+            'APPLICATION RECORDS');
+    });
 
-    // it('should successfully add a new application record', async () => {
-    //     await LandingPage.clickOnNavigationLink('Application Records');
-    //     await LandingPage.newEmployeeSubmissionLink.click();
-    //     await expect(NewEmployeePage.getField('First Name')).toBeExisting();
-    //     await NewEmployeePage.getField('First Name').setValue("Vijay");
-    //     await NewEmployeePage.getField('Last Name').setValue("Bhaskar");
-    //     await NewEmployeePage.getField('Street Address').setValue("Plot No:666,\n Flat No : 302, 3rd Floor,\n Aditya Residency,\n Road No:5/7, \n Matrusri Nagar,\n Hafeezpet, \n, Miyapur, \n,Hyderabad,\n Telangana, \n, 500049");
-    //     await NewEmployeePage.getField('City').setValue("Hyderabad");
-    //     await NewEmployeePage.getField('State').setValue("Telangana");
-    //     await NewEmployeePage.getField('Telephone').setValue("+91-8939742475");
-    //     await NewEmployeePage.getField('Zip').setValue("500049");
-    //     await NewEmployeePage.getField('Email').setValue("vijay.in.vnr@gmail.com");
-    //     //browser.moveToObject(NewEmployeePage.getField('Text'));
-    //     await NewEmployeePage.getField('Text').setValue("Sample Text");
-    //     var radiobox = await NewEmployeePage.getField('Full Time');
-    //     await radiobox.click();
-    //     radiobox = await NewEmployeePage.getField('Dental');
-    //     await radiobox.click();
-    //     radiobox = await NewEmployeePage.getField('Marketing');
-    //     await radiobox.click();
-    //     var dropdown = await NewEmployeePage.getField("Favourite Band");
-    //     await dropdown.click();
-    //     NewEmployeePage.selectDropdown("Luke Vibert");
-    //     await NewEmployeePage.getField("Notes").setValue("This is sample comments from automation");
-    //     await NewEmployeePage.getField("Post Comment").click();
-    //     await NewEmployeePage.getField("Save").click();
-    //     await NewEmployeePage.getField("Time Spent").click();
-    //     await NewEmployeePage.getField("Record Saved").waitForExist({ timeout: 5000 });
-    //     await NewEmployeePage.getField("Record Saved").click();
-    //     await browser.saveScreenshot('/Users/vijaym/Desktop/screenshot.png');
+    it('should successfully add a new application record', async () => {
+        await LandingPage.clickOnNavigationLink('Application Records');
+        await LandingPage.newEmployeeSubmissionLink.click();
+        await expect(NewEmployeePage.getField('First Name')).toBeExisting();
+        await NewEmployeePage.getField('First Name').setValue("Vijay");
+        await NewEmployeePage.getField('Last Name').setValue("Bhaskar");
+        await NewEmployeePage.getField('Street Address').setValue("Plot No:666,\n Flat No : 302, 3rd Floor,\n Aditya Residency,\n Road No:5/7, \n Matrusri Nagar,\n Hafeezpet, \n, Miyapur, \n,Hyderabad,\n Telangana, \n, 500049");
+        await NewEmployeePage.getField('City').setValue("Hyderabad");
+        await NewEmployeePage.getField('State').setValue("Telangana");
+        await NewEmployeePage.getField('Telephone').setValue("+91-8939742475");
+        await NewEmployeePage.getField('Zip').setValue("500049");
+        await NewEmployeePage.getField('Email').setValue("vijay.in.vnr@gmail.com");
+        //browser.moveToObject(NewEmployeePage.getField('Text'));
+        await NewEmployeePage.getField('Text').setValue("Sample Text");
+        var radiobox = await NewEmployeePage.getField('Full Time');
+        await radiobox.click();
+        radiobox = await NewEmployeePage.getField('Dental');
+        await radiobox.click();
+        radiobox = await NewEmployeePage.getField('Marketing');
+        await radiobox.click();
+        var dropdown = await NewEmployeePage.getField("Favourite Band");
+        await dropdown.click();
+        NewEmployeePage.selectDropdown("Luke Vibert");
+        await NewEmployeePage.getField("Notes").setValue("This is sample comments from automation");
+        await NewEmployeePage.getField("Post Comment").click();
+        await NewEmployeePage.getField("Save").click();
+        await NewEmployeePage.getField("Time Spent").click();
+        await NewEmployeePage.getField("Record Saved").waitForExist({ timeout: 5000 });
+        await NewEmployeePage.getField("Record Saved").click();
+        await browser.saveScreenshot('/Users/vijaym/Desktop/screenshot.png');
         
-    // });
+    });
 
 
     it('Swimlane API- fetch application by recordId', async() =>{
@@ -60,28 +60,24 @@ describe('Swimlane application', () => {
                 // parsed response body as js object
                 //console.log('Data is '+ JSON.parse(data).token);
                 token = JSON.parse(data).token;
+                console.log(token);
                 //console.log(response.statusCode);
                 if (parseInt(response.statusCode) === 200) {
-                   console.log("Res is"+response.data['token']);
+                   //console.log("Res is"+response.data['token']);
                 }
+
+                var args = {
+                    path: { "id": 1234 },
+                    headers: { "Content-Type": "application/json", "Authorization": "Bearer " + token},
+                    strictSSL: false
+                };
+                console.log('Sending the API request');
+                client.get("https://qa-practical.qa.swimlane.io/api/app/${id}/record", args,
+                    function (data, response) {
+                        console.log(response.statusCode);
+                    });
             });
-
-        console.log("Token is "+  token);
-        var args = {
-            path: { "id": 1234 },
-            headers: { "Content-Type": "application/json", "Authorization": "Bearer " + token},
-            strictSSL: false
-        };
-        console.log('Sending the API request');
-        var req  = client.get("https://qa-practical.qa.swimlane.io/api/app/${id}/record", args,
-        function (data, response) {
-            // parsed response body as js object
-            console.log('Data is '+ data);
-            // raw response
-            console.log('Response is ' + response);
-        });
-
-        req.on('error', function (err) {
+        request.on('error', function (err) {
             console.log('request error', err);
         });
 
